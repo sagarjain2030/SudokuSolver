11eac9390f4505eb8b9cb5078a30236b6e155a0a
sagarjain2030 <sagarjain2030@gmail.com>
2020-02-26 18:53:37 +0530

    Validating Sudoku input string for valid inputs

--- SudokuStringValidator.py
+++ SudokuStringValidator.py
@@ -6,3 +6,10 @@ class SudokuStringValidator(object):
         if(len(sudokuString) != 81):
             return False
         return True
+
+    def inputOfString(self,sudokuString):
+        possibleInputs = ['1','2','3','4','5','6','7','8','9','#']
+        for elem in list(sudokuString):
+            if elem not in possibleInputs:
+                return False
+        return True
--- test_SudokuStringValidator.py
+++ test_SudokuStringValidator.py
@@ -7,6 +7,17 @@ class Test_test_Sudoku(unittest.TestCase):
         s_test = SudokuStringValidator()
         self.assertTrue(s_test.checkLengthOfString(requestedString),msg='should return true')
 
+     def test_inputOfString001(self):
+        requestedString = '2#############62####1####7###6##8###3###9###7###6##4###4####8####52#############3'
+        s_test = SudokuStringValidator()
+        self.assertTrue(s_test.inputOfString(requestedString),msg='should return true')
+
+     def test_inputOfString002(self):
+        requestedString = '2##a##########62#$##1####7###6##8###3###9###7#0#6##4###4####8####52##B##########3'
+        s_test = SudokuStringValidator()
+        self.assertFalse(s_test.inputOfString(requestedString),msg='should return false')
+
+
 if __name__ == '__main__':
     unittest.main()
 
